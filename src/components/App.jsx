aeeb629696325213dc1aac4988f733b49f39aac2
ToporekDaniel <danieltoporek0@gmail.com>
2024-01-27 21:46:30 +0100

    step1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,43 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import styled from 'styled-components';
+import { NameInput } from './input/inputs';
+import { Component, useState } from 'react';
+
+const SubmitButton = styled.button`
+  width: 10rem;
+  height: 3rem;
+  background-color: white;
+  border-radius: 0.5rem;
+`;
+
+const Form = styled.form``;
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      contacts: [],
+      name: '',
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.name);
+  };
+
+  handleNameChange = event => {
+    this.setState({ name: event.target.value });
+  };
+
+  render() {
+    return (
+      <>
+        <Form onSubmit={this.handleSubmit}>
+          <NameInput onChange={this.handleNameChange} />
+          <SubmitButton type="submit">Submit</SubmitButton>
+        </Form>
+      </>
+    );
+  }
+}
---
+++ src/components/input/inputs.js
@@ -0,0 +1,14 @@
+export const NameInput = () => {
+  return (
+    <label>
+      Name
+      <input
+        type="text"
+        name="name"
+        pattern="^[a-z A-Z а-я А-Я]+(([' -][a-z A-Z а-я А-Я ])?[a-z A-Z а-я А-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+    </label>
+  );
+};
